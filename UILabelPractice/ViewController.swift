a3878ecd92d08602432eec7dd8f834174b46abb8
yuichi.yamada <yuichi513@gmail.com>
2020-02-23 14:54:35 +0900

    Initial Commit

---
+++ UILabelPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UILabelPractice
+//
+//  Created by 山田裕一 on 2020/02/23.
+//  Copyright © 2020 yuichi.yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

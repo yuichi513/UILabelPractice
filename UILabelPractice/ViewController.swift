42b2043c93ceec6749022af6a06148323e2fce51
yuichi.yamada <yuichi513@gmail.com>
2020-02-29 14:22:00 +0900

    UILabelPracticeを作成した。

--- UILabelPractice/ViewController.swift
+++ UILabelPractice/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func handle(_ sender: Any) {
+        label.text = "Swift"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
